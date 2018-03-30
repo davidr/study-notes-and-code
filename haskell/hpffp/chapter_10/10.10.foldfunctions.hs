24fd58eae5290f92aa4937ccefc4e34100937bbb
David Ressman <davidr@ressman.org>
2018-03-30 14:53:45 -0500

    Finish chapter 10 (whew)

--- haskell/hpffp/chapter_10/10.10.foldfunctions.hs
+++ haskell/hpffp/chapter_10/10.10.foldfunctions.hs
@@ -13,4 +13,28 @@ myElem :: Eq a => a -> [a] -> Bool
 myElem x = foldr (\a b -> if x == a then True else b) False
 
 myElemAny :: Eq a => a -> [a] -> Bool
-myElemAny = any (==)
+myElemAny x = any (== x)
+
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\a -> (:) $ f a) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr (\x xs -> if f x then x : xs else xs) []
+
+squish :: [[a]] -> [a]
+squish = foldr (\x y -> x ++ y) []
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr (\x y -> f x ++ y) []
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs) = foldr (\a b -> if f a b == GT then a else b) x xs
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x:xs) = foldr (\a b -> if f a b == LT then a else b) x xs
